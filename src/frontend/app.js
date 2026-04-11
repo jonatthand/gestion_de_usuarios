69730c5b216e6f9dfc13c6de2bd6646461690d39
Jonathan Daniel <jonatthand1@gmail.com>
2026-04-11 18:06:42 -0300

    backend deploy

--- src/frontend/app.js
+++ src/frontend/app.js
@@ -1,4 +1,7 @@
 const API = "http://127.0.0.1:8000";
+const API_URL = "https://gestion-de-usuarios-pums.onrender.com"
+
+fetch(`${API_URL}/notifications`)
 
 function showMessage(msg, type="success"){
     const el = document.getElementById("message");
