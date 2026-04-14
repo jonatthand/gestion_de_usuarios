cb62ede733d8762fc0086d0c34b855a75c5de6dc
Jonathan Daniel <jonatthand1@gmail.com>
2026-04-14 02:13:32 -0300

    fix cors

--- src/main.py
+++ src/main.py
@@ -1,9 +1,7 @@
 from fastapi import FastAPI
-from src.auth.routers.auth_router import router as auth_router
 from src.core.database import engine, Base
 from src.auth.models.user_model import User
 from src.notifications.models.notification_model import Notification
-from src.notifications.controllers.notification_router import router as notification_router
 from fastapi.middleware.cors import CORSMiddleware
 
 Base.metadata.create_all(bind=engine)
@@ -14,16 +12,24 @@ app = FastAPI(
         version="1.0.0"
 )
 
+origins = [
+    "https://frontend-notifications-eta.vercel.app",
+]
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://frontend-notifications-eta.vercel.app"],
+    allow_origins=origins,
     allow_credentials=True,
-    allow_methods=["*"],
+    allow_methods=["*"], 
     allow_headers=["*"],
 )
 
-app.include_router(notification_router, prefix="/notifications", tags=["Notifications"])
+from src.notifications.controllers.notification_router import router as notification_router
+from src.auth.routers.auth_router import router as auth_router
+
+
 app.include_router(auth_router, prefix="/auth", tags=["Auth"])
+app.include_router(notification_router, prefix="/notifications", tags=["Notifications"])
 
 
 
